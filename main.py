f091e6a78a01cfac67481ec61f511b409efa1281
kit64 <mrvadim2000@gmail.com>
2023-11-18 23:27:02 +0300

    init

---
+++ main.py
@@ -0,0 +1,123 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types, F
+
+import os
+import pytube
+from datetime import datetime
+
+import transcriber
+import utils
+
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+dp = Dispatcher()
+
+youtube_url_pattern = r"(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/(watch\?v=|embed\/)?([a-zA-Z0-9_-]{11})(\S*)?"
+time_pattern_optional = r"((\d)+:(\d){2}:(\d){2})?"  # H/HH:MM:SS format
+
+@dp.message(
+    F.text.regexp(
+        r"^"
+        + youtube_url_pattern
+        + r"(\s)?"
+        + time_pattern_optional
+        + r"(\s)?"
+        + time_pattern_optional
+        + r"$"
+    )
+)
+async def transcribe_video(message: types.Message):
+    print("Got the video!")
+    args = message.text.split(" ")
+    video_url = args[0]
+    print(f"video_url: {video_url}")
+    youtube_video = pytube.YouTube(video_url)
+
+    try:
+        input_start_time = args[1]
+        time_object = datetime.strptime(input_start_time, "%H:%M:%S")
+        start_time = time_object.hour * 3600 + time_object.minute * 60 + time_object.second
+        if start_time > youtube_video.length:
+            await message.answer("Время старта больше, чем длина всего видео 😲😲😲")
+            print("Failed to transcript the video: start_time > video.length")
+            return
+    except ValueError:
+        await message.answer(
+                "Формат времени для старта неверный\n"
+                "Поправьте его, пожалуйста, чтобы я cмог его понять ❤️\n"
+                "Я понимаю в формате: ЧЧ:ММ:СС или Ч:ММ:СС"
+            )
+        print("Failed to transcript the video: wrong time format for start_time ")
+        return
+    except IndexError:
+        start_time = 0
+    print(f"start_time: {start_time}")
+
+    try:
+        input_end_time = args[2]
+        time_object = datetime.strptime(input_end_time, "%H:%M:%S")
+        end_time = time_object.hour * 3600 + time_object.minute * 60 + time_object.second
+        if end_time > youtube_video.length:
+            await message.answer("Время финиша больше, чем длина всего видео 🤨")
+            print("Failed to transcript the video: end_time > video.length")
+            return
+        if start_time > end_time:
+            await message.answer(
+                "Хмм, кажется Вы что-то перепутали 🤔\n"
+                "У Вас время старта опережает время финиша 🧐"
+            )
+            print("Failed to transcript the video: start_time > end_time")
+            return
+    except ValueError:
+        await message.answer(
+                "Формат времени для финиша неверный\n"
+                "Поправьте его, пожалуйста, чтобы я cмог его понять ❤️\n"
+                "Я понимаю в формате: ЧЧ:ММ:СС или Ч:ММ:СС"
+            )
+        print("Failed to transcript the video: wrong format for end_time")
+        return
+    except IndexError:
+        end_time = youtube_video.length
+    print(f"end_time: {end_time}")
+
+    mp3_file_path = utils.get_mp3_from_youtube_video(
+        video_url, audio_folder_path="audio to transcribe"
+    )
+
+    if start_time == 0 and end_time == youtube_video.length:
+        try:
+            transcript = transcriber.transcribe(mp3_file_path, language="ru", format="mp3")
+        except:
+            await message.answer("Произошла ошибка, похоже, что сервис транскрипции OpenAI временно не доступен 😒")
+            return
+    else:
+        trimmed_mp3_file_path = utils.trim_audio(mp3_file_path, start_time, end_time, format="mp3")
+        try:
+            transcript = transcriber.transcribe(trimmed_mp3_file_path, language="ru", format="mp3")
+        except:
+            await message.answer("Произошла ошибка, похоже, что сервис транскрипции OpenAI временно не доступен 😒")
+            return
+        finally:
+            os.remove(trimmed_mp3_file_path)
+
+    mp3_file_name = os.path.basename(mp3_file_path)
+    transcript_file_name = os.path.splitext(mp3_file_name)[0] + ".txt"
+    transcript_file_path = utils.write_transcript_to_file(
+        transcript, folder_path="transcripts", file_name=transcript_file_name
+    )
+
+    with open(transcript_file_path, "r", encoding="utf-8") as transcript_file:
+        file_path = os.path.realpath(transcript_file.name)
+        file_id = types.FSInputFile(file_path)
+    await message.answer_document(file_id)
+
+
+async def main():
+    bot = Bot(BOT_TOKEN)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    print("Bot has been launched")
+    asyncio.run(main())
---
+++ transcriber.py
@@ -0,0 +1,78 @@
+import os
+from pydub import AudioSegment
+from pydub.utils import mediainfo
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+MAX_FILE_SIZE = 25  # MEGABYTES
+
+
+def split_audio(file_path, max_length, format="mp3"):
+    audio = AudioSegment.from_file(file_path, format=format)
+    length_audio = len(audio)
+    parts = int(length_audio / max_length) + 1
+    audio_chunks = [audio[i * max_length : (i + 1) * max_length] for i in range(parts)]
+    return audio_chunks
+
+
+def get_bitrate(file_path):
+    bitrate = int(mediainfo(file_path)["bit_rate"])
+    return bitrate
+
+
+def transcribe(audio_file_path: str, language="ru", format="mp3") -> str:
+    client = OpenAI(api_key=OPENAI_API_KEY)
+
+    file_size = os.path.getsize(audio_file_path)
+    print(f"The size of the audio file is {file_size / 1024:.2f} kilobytes")
+
+    if file_size < MAX_FILE_SIZE * 1024 * 1024:
+        try:
+            with open(audio_file_path, "rb") as audio_file:
+                full_transcript = client.audio.transcriptions.create(
+                    file=audio_file, model="whisper-1", language=language, response_format="text"
+                )
+        except Exception as e:
+            print(f"Error transcribing audio: {e}")
+            raise e
+        return full_transcript
+
+    bitrate_kbps = get_bitrate(audio_file_path) / 1024
+    print(f"Bitrate: {bitrate_kbps:.9f} kbps")
+    max_length_seconds = ((MAX_FILE_SIZE * 1024 * 8) / bitrate_kbps) - 1  # -1 second to ensure
+    max_length = int(max_length_seconds * 1000)  # measured in milliseconds
+    audio_chunks = split_audio(audio_file_path, max_length, format)
+    print(f"Number of chunks: {len(audio_chunks)}")
+
+    transcripts = []
+    for chunk_index, chunk in enumerate(audio_chunks):
+        temp_audio_file_path = f"temp_chunk_{chunk_index}.{format}"
+        chunk.export(temp_audio_file_path, format=format)
+        try:
+            with open(temp_audio_file_path, "rb") as audio_file:
+                response = client.audio.transcriptions.create(
+                    file=audio_file, model="whisper-1", language=language, response_format="text"
+                )
+                transcripts.append(response)
+        except Exception as e:
+            print(f"Error transcribing audio chunk #{chunk_index}: {e}")
+        finally:
+            os.remove(temp_audio_file_path)
+
+    full_transcript = " ".join(transcript.strip() for transcript in transcripts)
+    return full_transcript
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) != 2:
+        print("Usage: python transcriber.py <path_to_audio_file>")
+        sys.exit(1)
+
+    audio_file_path = sys.argv[1]
+    transcript = transcribe(audio_file_path)
+    print(transcript)
