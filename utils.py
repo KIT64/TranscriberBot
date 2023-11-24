7528bd695a75870f00d47237c72399b5310021dd
kit64 <mrvadim2000@gmail.com>
2023-11-25 00:29:26 +0300

    Fix invalid chars in filename error

--- utils.py
+++ utils.py
@@ -5,16 +5,21 @@ from pydub import AudioSegment
 
 def get_mp3_from_youtube_video(video_url, start_time, end_time, audio_folder_path="") -> str:
     video = pytube.YouTube(video_url)
-    mp4_audio_file_path = os.path.join(audio_folder_path, video.title + ".mp4")
-    mp3_file_path = os.path.join(audio_folder_path, video.title + ".mp3")
+    mp4_audio_file_path = os.path.join(audio_folder_path, sanitize_filename(video.title) + ".mp4")
+    mp3_file_path = os.path.join(audio_folder_path, sanitize_filename(video.title) + ".mp3")
 
     if os.path.exists(mp4_audio_file_path):
         print("MP4 file already exists. Skipping download.")
     else:
-        audio_stream = video.streams.get_audio_only()
-        print("Downloading the audio stream...")
-        mp4_audio_file_path = audio_stream.download(output_path=audio_folder_path)
-        print(f"Audio stream (mp4) downloaded to {mp4_audio_file_path}")
+        try:
+            audio_stream = video.streams.get_audio_only()
+            print("Downloading the audio stream...")
+            mp4_audio_file_path = audio_stream.download(output_path=audio_folder_path)
+            print(f"Audio stream (mp4) downloaded to {mp4_audio_file_path}")
+        except Exception as e:
+            os.remove(mp4_audio_file_path)
+            print(f"Error while attempting to download audio stream from youtube: {e}")
+            raise e
 
     print("Converting mp4 to mp3...")
     mp3_audio = AudioSegment.from_file(
@@ -32,3 +37,9 @@ def write_transcript_to_file(transcript, folder_path, file_name) -> str:
     with open(transcript_file_path, "w", encoding="utf-8") as transcript_file:
         transcript_file.write(transcript)
     return transcript_file_path
+
+def sanitize_filename(filename):
+    # Replace invalid characters with underscores
+    invalid_chars = {'.', '/'}
+    sanitized_filename = ''.join(char if char not in invalid_chars else '_' for char in filename)
+    return sanitized_filename
